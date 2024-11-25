a531f32683a1b6a1ffff19bba36a7ec9dad42e71
Cesar Garcia <celord@gmail.com>
2024-11-25 14:58:07 -0600

    first commit

---
+++ local_lms/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from local_lms.users.models import User
+from local_lms.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
