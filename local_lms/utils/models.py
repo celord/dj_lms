82c712e15c063bce37b783c477648077c8c0bf81
Cesar Garcia <celord@gmail.com>
2024-12-19 22:12:23 -0600

    organizations.

---
+++ local_lms/utils/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from colorhash import ColorHash
+from django.db import models
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def hex_color(self):
+        return ColorHash(self.uuid).hex
